e33de20715cf1bb72afde96edd9b2bd2ee5c8e53
julianjackson <20231881+isjulianj@users.noreply.github.com>
2025-12-18 16:05:32 +0000

    Final book bot from boot.dev

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from stats import get_word_count
-
+from stats import get_word_count, get_char_counts, get_sorted_char_dict 
+import sys
 
 def get_book_text(file_path):
     file_contents = ""
@@ -9,8 +9,29 @@ def get_book_text(file_path):
 
 
 def main():
-    contents = get_book_text("books/frankenstein.txt")
-    word_count = get_word_count(contents)
-    print(f"Found {word_count} total words")
+    args = sys.argv
+    if len(args) == 1 or len(args) > 2:
+        print("Usage: python3 main.py <path_to_book>")
+        quit(1)
 
+    path =  args[1]
+    contents = ""
+    try: 
+        contents = get_book_text(path)
+    except:
+        print("No book found matching that path")
+        quit(1)
+        
+    word_count = get_word_count(contents)
+    char_count = get_char_counts(contents) 
+    sorted_count = get_sorted_char_dict(char_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for dict in sorted_count:
+        if dict["char"].isalpha():
+            print(f"{dict["char"]}: {dict["nums"]}")
+    print("============= END ===============")
 main()    
--- stats.py
+++ stats.py
@@ -1,2 +1,27 @@
 def get_word_count(book):
     return len(book.split())
+
+def get_char_counts(book_text):
+    char_dict = {}
+    for char in book_text:
+        lower = char.lower()
+        if char_dict.get(lower):
+            char_dict[lower] = char_dict.get(lower) + 1
+        else:
+            char_dict[lower] = 1
+    return char_dict
+
+
+def get_sorted_char_dict(char_dict):
+    new_dict = []
+    for char in char_dict:
+        new_dict.append({"char": char, "nums": char_dict[char]})
+
+    def sort_on(items):
+        return items["nums"]
+
+    new_dict.sort(reverse=True, key=sort_on)
+    
+    return new_dict
+    
+
